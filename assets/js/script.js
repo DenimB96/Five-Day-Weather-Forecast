2537b55e23f6ffc797dca26626654724320a246b
DenimB96 <rlsjr2@live.com>
2023-02-08 15:30:52 -0500

    Finished HTML with the bootstrap id's and classes, finally have the API key, the structure is all here will add styling to current html before finshing the Javascript code in side folder. Sourced Jquery for Javascript as well..... I want to clean up div's will note to focus on that after Javascript is complete.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}    (Base Url for API)
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}   (Alternative API call)
+
+//let apiKey = e47712857159e20ca3b38193fa3a32e8   // Api Key for above calls
+

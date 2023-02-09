b424a3df916c8b4fad798aeb8b67c1b0e9bed12e
DenimB96 <rlsjr2@live.com>
2023-02-08 23:32:18 -0500

    updated CSS and made sure the 5 day forecast boxes were spaced from eachother, bringing up finished javascript after testing with comments, also got rid of extra comments and added in new comments. Unfortunately spent alot of time on getting the UVIndex but it wasnt required so its just a nice extra touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,169 @@
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}    (Base Url for API)
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}   (Alternative API call)
+// Variable declarations
+var city = "";
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty = $("#humidity");
+var currentWSpeed = $("#wind-speed");
+var currentUvindex = $("#uv-index");
+var sCity = [];
+// searches the city to see if it exists in the entries from the storage
+function find(c) {
+  for (var i = 0; i < sCity.length; i++) {
+    if (c.toUpperCase() === sCity[i]) {
+      return -1;
+    }
+  }
+  return 1;
+}
+//Set up the API key
+var APIKey = "e47712857159e20ca3b38193fa3a32e8";
+// Display the curent and future weather from input
+function displayWeather(event) {
+  event.preventDefault();
+  if (searchCity.val().trim() !== "") {
+    city = searchCity.val().trim();
+    currentWeather(city);
+  }
+}
+// Make the ajax call and build URL to get server side data
+function currentWeather(city) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&APPID=" +
+    APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // parse the response to display the current weather, the city name, date and the weather icon
+    console.log(response);
+    var weathericon = response.weather[0].icon;
+    var iconurl =
+      "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+    var date = new Date(response.dt * 1000).toLocaleDateString();
+    //parse the response for name of city and concanatig the date and icon.
+    $(currentCity).html(
+      response.name + "(" + date + ")" + "<img src=" + iconurl + ">"
+    );
 
-//let apiKey = e47712857159e20ca3b38193fa3a32e8   // Api Key for above calls
+    // parse the response to display the current temperature and convert temperature into farenheit, then display the humidity. Then display wind speed and convert it to MPH
+    var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+    $(currentTemperature).html(tempF.toFixed(2) + "&#8457");
+    $(currentHumidty).html(response.main.humidity + "%");
+    var ws = response.wind.speed;
+    var windsmph = (ws * 2.237).toFixed(1);
+    $(currentWSpeed).html(windsmph + "MPH");
 
+    // Display UVIndex and setup building query url
+    UVIndex(response.coord.lon, response.coord.lat);
+    forecast(response.id);
+    if (response.cod == 200) {
+      sCity = JSON.parse(localStorage.getItem("cityname"));
+      console.log(sCity);
+      if (sCity == null) {
+        sCity = [];
+        sCity.push(city.toUpperCase());
+        localStorage.setItem("cityname", JSON.stringify(sCity));
+        addToList(city);
+      } else {
+        if (find(city) > 0) {
+          sCity.push(city.toUpperCase());
+          localStorage.setItem("cityname", JSON.stringify(sCity));
+          addToList(city);
+        }
+      }
+    }
+  });
+}
+// UVIndex response and url build
+function UVIndex(ln, lt) {
+  var uvqURL =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+    APIKey +
+    "&lat=" +
+    lt +
+    "&lon=" +
+    ln;
+  $.ajax({
+    url: uvqURL,
+    method: "GET",
+  }).then(function (response) {
+    $(currentUvindex).html(response.value);
+  });
+}
+
+// 5 Day forecast
+function forecast(cityid) {
+  var dayover = false;
+  var queryforcastURL =
+    "https://api.openweathermap.org/data/2.5/forecast?id=" +
+    cityid +
+    "&appid=" +
+    APIKey;
+  $.ajax({
+    url: queryforcastURL,
+    method: "GET",
+  }).then(function (response) {
+    for (i = 0; i < 5; i++) {
+      var date = new Date(
+        response.list[(i + 1) * 8 - 1].dt * 1000
+      ).toLocaleDateString();
+      var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
+      var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+      var tempK = response.list[(i + 1) * 8 - 1].main.temp;
+      var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(2);
+      var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
+
+      $("#fDate" + i).html(date);
+      $("#fImg" + i).html("<img src=" + iconurl + ">");
+      $("#fTemp" + i).html(tempF + "&#8457");
+      $("#fHumidity" + i).html(humidity + "%");
+    }
+  });
+}
+
+//Dynamically add the city to the search history
+function addToList(c) {
+  var listEl = $("<li>" + c.toUpperCase() + "</li>");
+  $(listEl).attr("class", "list-group-item");
+  $(listEl).attr("data-value", c.toUpperCase());
+  $(".list-group").append(listEl);
+}
+// display the past search again when the city is clicked in search history
+function invokePastSearch(event) {
+  var liEl = event.target;
+  if (event.target.matches("li")) {
+    city = liEl.textContent.trim();
+    currentWeather(city);
+  }
+}
+
+// render function
+function loadlastCity() {
+  $("ul").empty();
+  var sCity = JSON.parse(localStorage.getItem("cityname"));
+  if (sCity !== null) {
+    sCity = JSON.parse(localStorage.getItem("cityname"));
+    for (i = 0; i < sCity.length; i++) {
+      addToList(sCity[i]);
+    }
+    city = sCity[i - 1];
+    currentWeather(city);
+  }
+}
+//Clear the search history from the page
+function clearHistory(event) {
+  event.preventDefault();
+  sCity = [];
+  localStorage.removeItem("cityname");
+  document.location.reload();
+}
+//Click Handlers
+$("#search-button").on("click", displayWeather);
+$(document).on("click", invokePastSearch);
+$(window).on("load", loadlastCity);
+$("#clear-history").on("click", clearHistory);
